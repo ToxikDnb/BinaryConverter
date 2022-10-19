e4602170ce032283f8d845b649bbbbb0177ed150
ToxikDnb <54173553+ToxikDnb@users.noreply.github.com>
2022-10-19 12:25:38 +0100

    Add files via upload

---
+++ main.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+
+#define mdp 36
+
+int main(){
+    int isRunning = 1;
+    while(isRunning){
+        printf("Please enter the number to convert to binary: ");
+        char c_response[mdp];
+        fgets(c_response, mdp, stdin);
+        if(!strcmp(c_response, "exit\n")){
+            isRunning = 0;
+        }
+        else{
+            puts("1");
+            int response = atoi(c_response);
+            int storedResponse = response;
+            int highestBinNum = 1;
+            while(highestBinNum<response){
+                highestBinNum*=2;
+            }
+            int amountOfDigits = log(highestBinNum)/log(2);
+            char num[amountOfDigits];
+            int nextFree = 0;
+            for(int i = 0; i <= amountOfDigits; i++){
+                if(response-highestBinNum >= 0){
+                    response -= highestBinNum;
+                    num[nextFree] = '1';
+                }
+                else{
+                    num[nextFree] = '0';
+                }
+                highestBinNum /= 2;
+                nextFree++;
+            }
+            num[nextFree] = '\0';
+            printf("\n\n%d in binary = %s\n", storedResponse, num);
+        }
+    }
+    return 0;
+}
